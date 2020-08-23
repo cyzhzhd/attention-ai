feat: gt calculation, fixed model

detect_model/anchor.py
@@ -3,24 +3,36 @@ import os
 
 # TODO: config to file
 CONFIG = {
+    "s_min": 0.1,
+    "s_max": 0.8,
     "anchor_num": [2, 6],
+    "total_anchor": 8,
     "cell_size": [16, 8],
     "output_path": "./"
 }
 
-# [num_box(896), 4(cx, cy, w, h)] anchor creation
-# w, h fixed to 1 (1:1 ratio + no size difference)
+# [num_box(896), 4(cx, cy, w, h)] anchor creation, save to .npy file
+# w, h ratio fixed to 1 (following blazeface paper)
 if __name__ == "__main__":
-    anchors = np.empty([0, 4], dtype=np.float32)
+    s_min = CONFIG['s_min']
+    s_max = CONFIG['s_max']
+    total_anchor = CONFIG['total_anchor']
     anchor_num = CONFIG['anchor_num']
     cell_size = CONFIG['cell_size']
 
+    anchors = np.empty([0, 4], dtype=np.float32)
+    cell_cumulated = 0
     for iteration, anchor in enumerate(anchor_num):
         cells = cell_size[iteration]
         for y in range(cells):
             for x in range(cells):
-                for _ in range(anchor):
+                for order in range(anchor):
+                    scale = s_min + (s_max - s_min) / \
+                        (total_anchor - 1) * (order + cell_cumulated)
                     anchors = np.vstack([anchors, np.array(
-                        [(1 * x + 0.5) / cells, (1 * y + 0.5) / cells, 1, 1])])
+                        [(1 * x + 0.5) / cells, (1 * y + 0.5) / cells, scale, scale])])
+        cell_cumulated += anchor_num[iteration]
 
+    print("last 10 results:")
+    print(anchors[-10:])
     np.save(os.path.join(CONFIG['output_path'], "anchors.npy"), anchors)

detect_model/loss.py
@@ -1,9 +1,7 @@
 import tensorflow as tf
 
 
-class MultiboxLoss(tf.keras.losses.Loss):
-    def __init__(self):
-        pass
-
-    def __call__(self, a, b):
-        print(a, b)
+def MultiboxLoss(true, pred):
+    tf.print(tf.keras.backend.shape(pred))
+    tf.print(tf.keras.backend.shape(true))
+    return tf.keras.backend.mean(tf.keras.backend.pow(pred - true, 2))

detect_model/models.py
@@ -1,103 +1,21 @@
+from backbones import *
 import tensorflow as tf
 
 
-class ChannelPad(tf.keras.layers.Layer):
-    def __init__(self):
-        super(ChannelPad, self).__init__()
-
-    def call(self, inputs, pad_num):
-        assert pad_num >= 0
-        if pad_num == 0:
-            return inputs
-        padding = tf.zeros_like(inputs)
-        padding = padding[:pad_num]
-        #pad_shape = tf.concat([inputs.shape[:-1], [pad_num]], axis=-1)
-        #padding = tf.zeros(pad_shape)
-        return tf.concat([inputs, padding], axis=-1)
-
-
-class BlazeBlock(tf.keras.Model):
-    def __init__(self, filters_1, filters_2=None, kernel_size=5,
-                 strides=1, padding='same', activation=tf.nn.relu):
-        super(BlazeBlock, self).__init__()
-        assert strides in [1, 2]
-        self.use_residual = (strides == 2)
-        self.use_double_block = filters_2 is not None
-        self.activation = tf.keras.layers.Activation(activation)
-
-        self.dw_conv_1 = tf.keras.Sequential([
-            tf.keras.layers.SeparableConv2D(
-                filters=filters_1,
-                kernel_size=kernel_size,
-                strides=strides,
-                padding=padding,
-            ),
-            tf.keras.layers.BatchNormalization()
-        ])
-        if self.use_double_block:
-            self.dw_conv_2 = tf.keras.Sequential([
-                tf.keras.layers.SeparableConv2D(
-                    filters=filters_2,
-                    kernel_size=kernel_size,
-                    strides=1,
-                    padding=padding,
-                ),
-                tf.keras.layers.BatchNormalization()
-            ])
-
-    def call(self, inputs, training=False):
-        x_0 = self.dw_conv_1(inputs)
-        if self.use_double_block:
-            x_0 = self.activation(x_0)
-            x_0 = self.dw_conv_2(x_0)
-        if self.use_residual:
-            pad_num = x_0.shape[-1] - inputs.shape[-1]
-            x_1 = tf.keras.layers.MaxPool2D()(inputs)
-            x_1 = ChannelPad()(x_1, pad_num)
-            x_0 = tf.keras.layers.Add()([x_0, x_1])
-        return self.activation(x_0)
-
-
-class Backbone(tf.keras.Model):
-    def __init__(self, activation=tf.nn.relu):
-        super(Backbone, self).__init__()
-        self.activation = activation
-
-    def call(self, inputs):
-        x_0 = tf.keras.layers.Conv2D(
-            filters=24, kernel_size=5, strides=2, padding='same')(inputs)
-        x_0 = tf.keras.layers.BatchNormalization()(x_0)
-        x_0 = tf.keras.layers.Activation(self.activation)(x_0)
-
-        x_0 = BlazeBlock(filters_1=24)(x_0)
-        x_0 = BlazeBlock(filters_1=24)(x_0)
-        x_0 = BlazeBlock(filters_1=24, strides=2)(x_0)
-        x_0 = BlazeBlock(filters_1=48)(x_0)
-        x_0 = BlazeBlock(filters_1=48)(x_0)
-
-        x_0 = BlazeBlock(filters_1=24, filters_2=96, strides=2)(x_0)
-        x_0 = BlazeBlock(filters_1=24, filters_2=96)(x_0)
-        x_0 = BlazeBlock(filters_1=24, filters_2=96)(x_0)
-        x_1 = BlazeBlock(filters_1=24, filters_2=96, strides=2)(x_0)
-        x_1 = BlazeBlock(filters_1=24, filters_2=96)(x_1)
-        x_1 = BlazeBlock(filters_1=24, filters_2=96)(x_1)
-
-        return x_0, x_1
-
-
-class Blazeface(tf.keras.Model):
+class Blazeface():
     def __init__(self, input_dim=(128, 128, 3), activation=tf.nn.relu):
-        super(Blazeface, self).__init__()
-        self.activation = activation
-        self.input_dim = input_dim
-
         # TODO: make configurable
         self.anchor_num = [2, 6]
         self.cell_size = [16, 8]
 
-    def call(self, inputs):
-        inputs = tf.keras.layers.Input(shape=self.input_dim)
-        feat_map_0, feat_map_1 = Backbone(activation=self.activation)(inputs)
+        self.activation = activation
+        self.input_dim = input_dim
+        self.backbone = blaze_backbone(
+            input_dim=input_dim)
+
+    def build_model(self):
+        backbone = self.backbone
+        feat_map_0, feat_map_1 = backbone.output
 
         x_0 = tf.keras.layers.Conv2D(filters=self.anchor_num[0], kernel_size=3,
                                      padding='same', activation='sigmoid')(feat_map_0)
@@ -121,4 +39,5 @@ class Blazeface(tf.keras.Model):
             [self.cell_size[1]**2 * self.anchor_num[1], 4])(x_3)
         bboxes = tf.concat([x_2, x_3], axis=1)
 
-        return tf.concat([confidences, bboxes], axis=-1)
+        predictions = tf.concat([confidences, bboxes], axis=-1)
+        return tf.keras.models.Model(backbone.inputs, predictions)

detect_model/train.py
@@ -1,4 +1,4 @@
-from widerface_loader import load_widerface
+from widerface_loader import load_widerface, generate_gt
 from models import Blazeface
 from loss import MultiboxLoss
 import tensorflow as tf
@@ -19,19 +19,19 @@ WIDER_GT = "/home/cyrojyro/hddrive/wider_face_split/wider_face_train_bbx_gt.txt"
 WIDER_TRAIN = "/home/cyrojyro/hddrive/WIDER_train/images"
 
 
-def dataloader(images, labels, batch_size=64):
+def dataloader(images, ground_truths, batch_size=64):
     data_keys = np.arange(len(images))
     while True:
         selected_keys = np.random.choice(
             data_keys, replace=False, size=batch_size)
 
         image_batch = []
-        label_batch = []
+        gt_batch = []
         for key in selected_keys:
             image_batch.append(images[key])
-            label_batch.append(labels[key])
-
-        yield (np.array(image_batch, dtype=np.float32), label_batch)
+            gt_batch.append(ground_truths[key])
+        yield (np.array(image_batch, dtype=np.float32),
+               np.array(gt_batch, dtype=np.float32))
 
 
 if __name__ == "__main__":
@@ -41,6 +41,13 @@ if __name__ == "__main__":
     # [num_picture, width(128), height(128), 3], [num_picture, num_gt, 4]
     images, labels = load_widerface(WIDER_GT, WIDER_TRAIN)
 
+    # [num_box(896), 4]
+    anchors = np.load(os.path.join(CONFIG['output_path'], "anchors.npy"))
+
+    # [num_labels, num_boxes, 5(responsible, tcx, tcy, tw, th)]
+    ground_truths = generate_gt(
+        labels, anchors, 0.3, CONFIG['anchor_num'], CONFIG['cell_size'])
+
     """ train validation split
     num_images = images.shape[0]
     num_train = int(num_images * CONFIG['train_ratio'])
@@ -49,17 +56,14 @@ if __name__ == "__main__":
     val_images, val_labels = images[num_train:], labels[num_train:]
     """
 
-    # [num_box(896), 4]
-    anchors = np.load(os.path.join(CONFIG['output_path'], "anchors.npy"))
-
     # DO TRAIN
-    model = Blazeface(input_dim=(128, 128, 3))
-    loss = MultiboxLoss()
+    model = Blazeface(input_dim=(128, 128, 3)).build_model()
+    loss = MultiboxLoss
     optim = tf.keras.optimizers.Adam()
     model.compile(loss=loss, optimizer=optim)
 
-    data_loader = dataloader(images, labels)
+    data_loader = dataloader(images, ground_truths)
     res = model.fit(x=data_loader,
-                    steps_per_epoch=100,
-                    epochs=10,
+                    steps_per_epoch=200,
+                    epochs=100,
                     verbose=1)

detect_model/widerface_loader.py
@@ -1,4 +1,4 @@
-from utils import drawplt
+from utils import drawplt, calc_iou_batch
 import tensorflow as tf
 import numpy as np
 import sys
@@ -29,7 +29,7 @@ def load_widerface(gt_dir, train_dir, target_w=128, target_h=128,
     """
     loads widerface dataset from directory. filter out images with small faces.
     returns: [num_picture, width(128), height(128), 3], [num_picture, num_gt, 4] 
-    (cx, cy, w, h - gt pixel location of resized image)
+    4(cx, cy, w, h - ratio of pixel location relative to resized image)
     """
     images, labels = [], []
     print('Processing dataset...')
@@ -52,8 +52,8 @@ def load_widerface(gt_dir, train_dir, target_w=128, target_h=128,
 
             image_path = os.path.join(train_dir, image_name)
             image = np.array(tf.keras.preprocessing.image.load_img(image_path))
-            scale_x = image.shape[1] / target_w
-            scale_y = image.shape[0] / target_h
+            scale_x = image.shape[1]
+            scale_y = image.shape[0]
 
             # scale gt
             label = []
@@ -65,7 +65,8 @@ def load_widerface(gt_dir, train_dir, target_w=128, target_h=128,
                 gt[1], gt[3] = gt[1] / scale_y,  gt[3] / scale_y
 
                 # filter out invalid or small gt boxes
-                if min(gt[2], gt[3]) > min_face_size and gt[7] != 1:
+                if (min(gt[2] * target_w, gt[3] * target_h) > min_face_size and
+                        gt[7] != 1):
                     label.append(np.array(gt[:4]))
                 else:
                     filter_flag = True
@@ -80,10 +81,42 @@ def load_widerface(gt_dir, train_dir, target_w=128, target_h=128,
             if label.size > 0:
                 images.append(image_normalized)
                 labels.append(label)
-                # drawplt(image_normalized, label)
+                # show normalized image and bbox
+                # drawplt(image_normalized, label, target_w, target_h)
 
         images = np.array(images)
         labels = np.array(labels)
         print('\nLoaded: ', images.shape, labels.shape)
 
         return images, labels
+
+
+def generate_gt(labels, anchors, iou_threshold=0.3,
+                anchor_num=[2, 6], cell_size=[16, 8]):
+    """
+    labels: [num_picture, num_gt, 4]]
+    anchors: [num_box(896), 4(cx, cy, w, h)]
+    returns: [num_labels, num_boxes, 5(responsible, tcx, tcy, tw, th)]
+    """
+    num_boxes = 0
+    process_num = 0
+    for i, anchor in enumerate(anchor_num):
+        num_boxes += (anchor * (cell_size[i] ** 2))
+    gts = np.empty([0, num_boxes, 5])
+
+    print('Processing gt...')
+    for label in labels:
+        process_num = print_progress(process_num)
+        gt = np.zeros([num_boxes, 5], dtype=np.float32)
+        for box in label:
+            ious = np.array(calc_iou_batch(box, anchors))
+            maxarg = ious > iou_threshold
+            gt[maxarg, 0] = 1.0
+            gt[maxarg, 1:3] = (box[0:2] - anchors[maxarg, 0:2]
+                               ) / anchors[maxarg, 0:2]
+            gt[maxarg, 3:5] = np.log(box[2:4] / anchors[maxarg, 2:4])
+        gts = np.vstack([gts, np.expand_dims(gt, axis=0)])
+
+    print(gts)
+    print(gts.shape)
+    return gts