e16ca961802cf24b7aad5c929fa05cc403927caa
Jae Gwon Choi <cyrojyro@gmail.com>
2020-09-02 21:57:09 +0900

    feat: yaw, roll, pitch feature added, new detector

--- src/landmark.js
+++ src/landmark.js
@@ -1,43 +1,63 @@
 import * as tfjs from "@tensorflow/tfjs";
 
 class LandmarkModel {
-  constructor() {
+  constructor(imgW, imgH) {
     this.model = null;
-    this.loadModel = this.loadFromUri.bind(this);
-    this.execute = this.execute.bind(this);
+    this.imgW = imgW;
+    this.imgH = imgH;
   }
 
   async loadFromUri(url) {
-    await tfjs.loadGraphModel(url).then((loadedModel) => {
-      this.model = loadedModel;
-    });
-    return new Promise((resolve) => {
-      resolve();
-    });
+    this.model = await tfjs.loadGraphModel(url);
   }
 
-  execute(croppedFace, outNode) {
-    if (croppedFace === undefined) return undefined;
-    return this.model.execute(croppedFace, outNode);
+  async predict(bbox, img) {
+    const croppedFace = this.cropFace(bbox, img);
+    if (croppedFace === undefined) return [undefined, undefined];
+
+    const [angles, _rawLandmarks] = this.model.execute(croppedFace, [
+      "Identity_1",
+      "Identity_2",
+    ]);
+    const angle = angles.arraySync();
+    const rawLandmarks = _rawLandmarks.arraySync();
+    const landmarks = convertLandmark(rawLandmarks, bbox);
+    tfjs.dispose([croppedFace, angles, landmarks]);
+    return [angle, landmarks];
+  }
+
+  cropFace(bbox, img) {
+    if (bbox === undefined) return undefined;
+    return tfjs.image.cropAndResize(
+      img,
+      [
+        [
+          bbox[1] / this.imgH,
+          bbox[0] / this.imgW,
+          bbox[3] / this.imgH,
+          bbox[2] / this.imgW,
+        ],
+      ],
+      [0],
+      [160, 160]
+    );
   }
 }
 
-export async function convertLandmark(landmarks, box) {
-  if (landmarks === undefined) return undefined;
+export function convertLandmark(landmarks, box) {
+  if (box === undefined) return undefined;
 
-  const _landmarks = await landmarks.array();
   let landmarkObj = new Array();
   for (let i = 0; i < 136; ++i) {
     if (!(i % 2)) landmarkObj[Math.floor(i / 2)] = new Object();
 
     let o =
       i % 2
-        ? { key: "_y", offset: box.y, mult: box.height }
-        : { key: "_x", offset: box.x, mult: box.width };
-    landmarkObj[Math.floor(i / 2)][o.key] =
-      o.offset + _landmarks[0][i] * o.mult;
+        ? { key: "_y", offset: box[1], mult: box[3] - box[1] }
+        : { key: "_x", offset: box[0], mult: box[2] - box[0] };
+    landmarkObj[Math.floor(i / 2)][o.key] = o.offset + landmarks[0][i] * o.mult;
   }
   return landmarkObj;
 }
 
-export const landmarkModel = new LandmarkModel();
+export const landmarkModel = new LandmarkModel(640, 480);
